8f2fbfb0db8137b854d7fbd728f2794a5b6f1e95
hongduck710 <hongik710@gmail.com>
2024-11-18 12:20:13 +0900

    헤더 네비게이션 코딩 진행중

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,35 @@ import  "./index.css";
 const GlobalStyles = createGlobalStyle`
   ${reset};
   @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
-  *{margin: 0; padding: 0; box-sizing:border-box;}; 
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+
+  *{margin: 0; padding: 0; box-sizing:border-box;}
   a{text-decoration:none;}
   ul,li{list-style:none;}
   body{font-size:16px;}
+  .w1500{max-width:1500px; width:100%; margin:auto;}
 
 
-  nav{width: 100%; }
+  nav{width: 100%; box-shadow: 0 0px 4px rgba(0,0,0,0.23);}
   nav ul.color-deco{width:100%; margin:auto; display:flex; box-shadow: 0 0px 4px rgba(0,0,0,0.23);}
   nav ul.color-deco li{height: 13px; width:33.333%; }
   nav ul.color-deco li.white{background-color:#fff;}
   nav ul.color-deco li.red{background-color:#FE4C4C;}
   nav ul.color-deco li.blue{background-color:#6F6AFF;}
-  nav a{color: #1C1C1D;margin: auto; font-size: 39px;font-family: "Playfair Display", serif;}
+
+  nav .w1500{display: flex; flex-wrap: wrap; align-items:center; justify-content:space-between;padding: 28px 0;}
+  nav a{color: #1C1C1D; font-family: 'Pretendard-Regular';}
+  nav .logo a{font-size: 39px;font-family: "Playfair Display", serif;}
   nav a span:first-child{color:#EF2C2C;}
   nav a span:last-child{color: #1e1ee2;}
 
-  nav .menus a{}
+  nav .menus a{color:#333; font-size:17px; font-weight: 400; margin-right:58px;}
+  nav .menus a:last-child{margin-right:0;}
 
 `;
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,14 +10,18 @@ const Navbar = () => {
                 <li className="white"></li>
                 <li className="red"></li>
             </ul>
-            <div className="menus">
-            <NavLink to="/"><span>A</span>llIANCE <span>F</span>RANÇAISE</NavLink>
-            <NavLink>소개</NavLink>
-            <NavLink>수강신청</NavLink>
-            <NavLink>시험안내</NavLink>
-            <NavLink>번역</NavLink>
-            <NavLink>공지</NavLink>
-            <NavLink>자주묻는 질문</NavLink>
+            <div className="w1500">
+                <div className="logo">
+                    <NavLink to="/"><span>A</span>llIANCE <span>F</span>RANÇAISE</NavLink>
+                </div>
+                <div className="menus">
+                <NavLink>소개</NavLink>
+                <NavLink>수강신청</NavLink>
+                <NavLink>시험안내</NavLink>
+                <NavLink>번역</NavLink>
+                <NavLink>공지</NavLink>
+                <NavLink>자주묻는 질문</NavLink>
+                </div>
             </div>
         </nav>
     );
